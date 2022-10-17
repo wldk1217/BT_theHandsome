fd3c740dd2216e3da29e40fdaa274991e7bb006b
Minseon Kim <75413136+Hozzinuna@users.noreply.github.com>
2022-10-17 15:20:33 +0900

    # WARNING: head commit changed in the meantime
    event view and insert coupon commit

---
+++ theHandsome/src/main/java/com/thehandsome/mapper/CouponMapper.java
@@ -0,0 +1,13 @@
+// 김민선 생성
+package com.thehandsome.mapper;
+
+import com.thehandsome.domain.CouponVO;
+
+// 쿠폰과 관련한 SQL을 호출하기 위한 인터페이스
+
+
+public interface CouponMapper {
+
+	public void makeCoupon(CouponVO coupon);
+	
+}
